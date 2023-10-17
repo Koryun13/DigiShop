a75ac7810366fe01e19b12182535483785db25c8
Koryun <Koryun.avanesyan@gmail.com>
2023-10-17 23:36:40 +0400

    Added Category View

--- DigiShop/Controllers/CategoryController.cs
+++ DigiShop/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DigiShop.Data;
+using DigiShop.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DigiShop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
         }
     }
 }
