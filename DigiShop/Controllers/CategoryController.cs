18bc581370b293302b7ca7ecc54d4ed4e8230843
Artash <artashesarshakyan@gmail.com>
2023-11-30 20:58:18 +0400

    edit delete update button added for category list

--- DigiShop/Controllers/CategoryController.cs
+++ DigiShop/Controllers/CategoryController.cs
@@ -2,36 +2,96 @@
 using DigiShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DigiShop.Controllers
+namespace DigiShop.Controllers;
+
+public class CategoryController : Controller
 {
-    public class CategoryController : Controller
+    private readonly ApplicationDbContext _db;
+    
+    public CategoryController(ApplicationDbContext db)
     {
-        private readonly ApplicationDbContext _db;
-        
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
+        _db = db;
+    }
 
-        public IActionResult Index()
-        {
-            List<Category> objCategoryList = _db.Categories.ToList();
+    public IActionResult Index()
+    {
+        List<Category> objCategoryList = _db.Categories.ToList();
 
-            return View(objCategoryList);
-        }
-     
+        return View(objCategoryList);
+    }
+ 
 
-        public IActionResult Create()
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Category cateoryOrder)
+    {
+        //_db.Categories.Add(cateoryOrder);
+        //_db.SaveChanges();
+        //return RedirectToAction("Index","Category");
+        if (cateoryOrder.Name == cateoryOrder.DisplayOrder.ToString())
         {
-            return View();
+            ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
         }
-        [HttpPost]
-        public IActionResult Create(Category cateoryOrder)
+        if (ModelState.IsValid)
         {
             _db.Categories.Add(cateoryOrder);
             _db.SaveChanges();
-            return RedirectToAction("Index","Category");
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
         }
+        return View();
+    }
 
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Category? categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category edited successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Category? categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Category obj)
+    {
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
     }
 }
